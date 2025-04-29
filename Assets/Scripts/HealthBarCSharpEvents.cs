bd368ac1960707e2ae24478aa04425a3c721478a
Ole <ole.juergensen87@gmail.com>
2025-04-29 13:22:58 +0200

    Adds health system with C# events
    Implements a health system utilizing C# events for managing health changes.
    
    This system includes:
    - IHealth interface defining core health properties and actions
    - HealthCSharpEvents class implementing IHealth with C# events for damage, healing, and health change notifications
    - HealthBarCSharpEvents component to update a health bar view based on the HealthCSharpEvents
    - A prefab for easy instantiation.

---
+++ Assets/Scripts/HealthBarCSharpEvents.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace GameProgramming1
+{
+    /// <summary>
+    /// Component for connecting a <see cref="HealthCSharpEvents"/> with a <see cref="HealthBarView"/>.
+    /// </summary>
+    public class HealthBarCSharpEvents : MonoBehaviour
+    {
+        [SerializeField]
+        private HealthCSharpEvents _health; // we cannot use the interface type here. It is not serializable
+        [SerializeField] private HealthBarView _view;
+
+        private void Awake()
+        {
+            if (_health == null)
+            {
+                Debug.LogWarning("No health script referenced for health bar. Disabling", this);
+                enabled = false;
+            }
+
+            if (_view == null)
+            {
+                Debug.LogWarning("No view component referenced. Disabling.", this);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _health.HealthChanged += OnHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            _health.HealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged(int newHealth)
+        {
+            _view.SetPercentage(100f * newHealth / _health.MaxHealth);
+        }
+    }
+}
