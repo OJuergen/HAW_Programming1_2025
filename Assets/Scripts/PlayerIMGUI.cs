ec85a3838aaeca85e35a4a3998c02759ffa6cf9e
Ole <ole@crumbling-game.com>
2025-05-08 09:19:43 +0200

    Move player imgui script to scripts folder

---
+++ Assets/Scripts/PlayerIMGUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace GameProgramming1
+{
+    public class PlayerIMGUI : MonoBehaviour
+    {
+        [SerializeField, Range(0, 100)] private int _amount = 5;
+
+        private IHealth _health;
+
+        private void Awake()
+        {
+            _health = GetComponentInChildren<IHealth>();
+        }
+
+        private void OnGUI()
+        {
+            if (_health != null && GUILayout.Button($"Take {_amount} Damage"))
+            {
+                _health.TakeDamage(_amount);
+            }
+
+            if (_health != null && GUILayout.Button($"Heal {_amount}"))
+            {
+                _health.Heal(_amount);
+            }
+        }
+    }
+}
